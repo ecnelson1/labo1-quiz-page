33f7374c4e649b16c30a3b6c9a00aee1f82a3217
Eion Nelson <ecnelson1@live.com>
2021-01-11 15:48:06 -0800

    added alert for te3st and 3 prompted questions

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ const resultsSection = document.getElementById('testRessults');
 // set event listeners to update state and DOM
 testButton.addEventListener('click', ()=>{ 
     console.log('It works');
-
+    alert('Welcome to THE BAT-TEST');
+    const canConfirm = confirm ('Are you sure you want to take THE BAT-TEST?');
+    console.log(canConfirm);
+    const answer1 = prompt('Batmans secret identity is Bruce Wayne?');
+    const answer2 = prompt('Does Batman work in Gotham City?')
+    const answer3 = prompt('Is Batman also known as The Worlds Greatest Detective?');
 });
